0a9cf67311c8fdbfe0e277c6cbce84f2febae04c
Aeonoi <dzhou527@gmail.com>
2026-01-25 16:25:19 -0500

    Clean up code

--- nvfans/src/main.rs
+++ nvfans/src/main.rs
@@ -1,17 +1,20 @@
-use nvfans_daemon;
 use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<_> = env::args().collect();
 
     if args.len() == 1 {
-        return Err("Needs an argument".into());
+        return Err("Requires an command argument.".into());
     }
 
     let command = args[1].clone();
     match command.as_str() {
         "daemon" => nvfans_daemon::run()?,
-        _ => println!("{command}"),
+        _ => {
+            eprintln!("Unrecognized command: {command}");
+            eprintln!("Usage: nvfans <command>");
+            return Err("Unrecognized command".into());
+        }
     }
 
     Ok(())
